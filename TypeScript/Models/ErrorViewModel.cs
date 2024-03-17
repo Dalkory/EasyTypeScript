00bf5ed5a18c71ea5f92abedc3cf813dc627ba4e
Даниил Таушканов <dtauskanov3@gmail.com>
2024-03-17 10:53:55 +0300

    Добавьте файлы проекта.

---
+++ TypeScript/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TypeScript.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
